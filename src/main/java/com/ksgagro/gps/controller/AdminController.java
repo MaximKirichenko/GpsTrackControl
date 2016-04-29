16cac6b6b98e89746aaaf8238773ce12335ca540
Maxim Kirichenko <kma280286@gmail.com>
2016-04-29 15:11:10 +0300

    add menu in admin page and add addFieldPage

--- src/main/java/com/ksgagro/gps/controller/AdminController.java
+++ src/main/java/com/ksgagro/gps/controller/AdminController.java
@@ -4,11 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ksgagro.gps.domain.MenuItem;
+
 @Controller
 public class AdminController {
 	@RequestMapping("/adminPage")
 	ModelAndView buildAdminPage(){
 		ModelAndView model = new ModelAndView("adminPage");
+		MenuItem[] menuItems = {new MenuItem("Добавление полей", "getAddFieldPage"), new MenuItem("Работа с тарировочными таблицами", "getCalibratioTablePage")};
+		model.addObject("menuItems", menuItems);
+		return model;
+	}
+	
+	@RequestMapping("/getAddFieldPage")
+	ModelAndView buildAddFieldsPage(){
+		ModelAndView model = new ModelAndView("addFieldsPage");
+		MenuItem[] menuItems = {new MenuItem("Добавление полей", "/getAddFieldPage"), new MenuItem("Работа с тарировочными таблицами", "/getCalibratioTablePage")};
+		model.addObject("menuItems", menuItems);
 		return model;
 	}
 
