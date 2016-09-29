e4af90bc1b77eb4eea806e33616826071b08620a
Maxim Kirichenko <kma280286@gmail.com>
2016-09-29 14:10:32 +0300

    Create test Controller

--- src/main/java/com/ksgagro/gps/tracks/menu/dto/VehicleDTO.java
+++ src/main/java/com/ksgagro/gps/tracks/menu/dto/VehicleDTO.java
@@ -1,41 +1,18 @@
 package com.ksgagro.gps.tracks.menu.dto;
 
-import com.ksgagro.gps.tracks.menu.utils.DateUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 /**
  * Created by kma on 29.09.2016.
  */
+@Component
 public class VehicleDTO {
-    private Date lastSignalDate;
-    private String vehicleName;
-    private String vehicleLocation;
-    @Autowired DateUtils dateUtils;
+    private Date lastSignalDate = new Date();
+    private String vehicleName = "Name";
+    private String vehicleLocation = "Location";
 
-    static enum VehicleStatus {MOVE("in move img"), ONLINE("online img"), OFFLINE("of line img");
-        String status;
-        VehicleStatus(String status) {
-            this.status = status;
-        }
-
-        private String getStatus() {
-            return status;
-        }
-    };
-
-    public VehicleStatus getCurrentStatus() {
-        Date currentDate = new Date();
-        if(currentDate.getTime()-lastSignalDate.getTime()>dateUtils.getLongPeriod(5, DateUtils.TimeMeasure.MINUTES)){
-            System.out.println(VehicleStatus.OFFLINE);
-            return VehicleStatus.OFFLINE;
-        }
-        else{
-            System.out.println(VehicleStatus.ONLINE);
-            return VehicleStatus.ONLINE;
-        }
-    }
     public Date getLastSignalDate() {
         return lastSignalDate;
     }
@@ -59,4 +36,39 @@ public class VehicleDTO {
     public void setVehicleLocation(String vehicleLocation) {
         this.vehicleLocation = vehicleLocation;
     }
+
+    //    @Autowired DateUtils dateUtils;
+//
+//    public VehicleStatus initCurrentStatus() {
+//        Date currentDate = new Date();
+//        if(currentDate.getTime()-lastSignalDate.getTime()>dateUtils.getLongPeriod(5, DateUtils.TimeMeasure.MINUTES)){
+//            return VehicleStatus.OFFLINE;
+//        }
+//        else{
+//            return VehicleStatus.ONLINE;
+//        }
+//    }
+//    public Date getLastSignalDate() {
+//        return lastSignalDate;
+//    }
+//
+//    public void setLastSignalDate(Date lastSignalDate) {
+//        this.lastSignalDate = lastSignalDate;
+//    }
+//
+//    public String getVehicleName() {
+//        return vehicleName;
+//    }
+//
+//    public void setVehicleName(String vehicleName) {
+//        this.vehicleName = vehicleName;
+//    }
+//
+//    public String getVehicleLocation() {
+//        return vehicleLocation;
+//    }
+//
+//    public void setVehicleLocation(String vehicleLocation) {
+//        this.vehicleLocation = vehicleLocation;
+//    }
 }
